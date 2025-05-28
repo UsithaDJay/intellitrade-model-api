73c0e3c9d4974e737a645d4c9f0ef198df93a775
UsithaDJay <usitha.20@cse.mrt.ac.lk>
2025-05-29 01:02:32 +0530

    removes unnecessary files

--- services/service_compute_features.py
+++ services/service_compute_features.py
@@ -30,32 +30,7 @@ class ComputeFeaturesService:
 
         # Select only the required input features
         final_df = filter_columns(df)
+        # print(final_df.iloc[-1:].to_string())
 
         return final_df.iloc[-1:]  # return the row for reference_date
-
-    # def compute_features(input_data: PredictRequestSchema) -> pd.DataFrame:
-    #     # TODO: Implement the feature computation logic
-    #     # feature list
-    #     features = input_data.raw_data
-    #     # df = pd.DataFrame([features], columns=["MA_21", "feature2", "feature3"])
-    #     # features = [
-    #     #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-    #     #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-    #     #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-    #     #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-    #     #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-    #     #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-    #     #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-    #     #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-    #     #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, None
-    #     # ]
-
-    #     # Convert to NumPy array and reshape
-    #     input_feature_array = np.array(features).reshape(1, -1)     # shape: (1, 90)
-
-    #     # Ensure the DataFrame has the correct shape and columns
-    #     # if df.shape[1] != 3:
-    #     #     raise ValueError("Invalid number of features provided.")
-        
-    #     # Add any additional feature engineering steps here
-    #     return input_feature_array
+    
