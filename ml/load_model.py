219dab3326b1cbe5bda141ba18a91f157316c254
UsithaDJay <usitha.20@cse.mrt.ac.lk>
2025-05-28 06:10:03 +0530

    Changes folder name

--- ml/load_model.py
+++ ml/load_model.py
@@ -1,6 +1,6 @@
 import pickle
 
-def load_model(model_path="xgb_model.pkl"):
+def load_model(model_path="ml/model/xgb_model.pkl"):
     with open(model_path, "rb") as f:
         model = pickle.load(f)
     return model
--- services/service_model.py
+++ services/service_model.py
@@ -1,5 +1,5 @@
-from model.load_model import load_model
-from model.predict import predict
+from ml.load_model import load_model
+from ml.predict import predict
 from schemas.schema_predict import PredictRequestSchema, PredictResponseSchema
 from services.service_compute_features import ComputeFeaturesService
 from fastapi import HTTPException
@@ -9,8 +9,8 @@ class ModelService:
         self.model = load_model()
 
     def predict(self, raw_data: PredictRequestSchema) -> PredictResponseSchema:
-        features = ComputeFeaturesService.compute_features(raw_data)
-        if not features:
-            raise HTTPException(status_code=400, detail="Invalid features provided.")
+        features = ComputeFeaturesService.compute_features(raw_data=raw_data)
+        # if features.any() is None or features.shape[1] != 90:
+        #     raise HTTPException(status_code=400, detail="Invalid features provided.")
         prediction = predict(self.model, features)
         return prediction
