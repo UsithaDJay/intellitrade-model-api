bdf6071700ceb7caed82a38bd03ac93307f995fe
UsithaDJay <usitha.20@cse.mrt.ac.lk>
2025-05-28 06:08:36 +0530

    sample input feature added

--- services/service_compute_features.py
+++ services/service_compute_features.py
@@ -1,16 +1,32 @@
 from schemas.schema_predict import PredictRequestSchema
 
 import pandas as pd
+import numpy as np
 
 class ComputeFeaturesService:
-    def compute_features(self, raw_data: PredictRequestSchema) -> pd.DataFrame:
+    def compute_features(raw_data: PredictRequestSchema) -> pd.DataFrame:
         # TODO: Implement the feature computation logic
+        # feature list
         features = raw_data.raw_data
-        df = pd.DataFrame([features], columns=["MA_21", "feature2", "feature3"])
+        # df = pd.DataFrame([features], columns=["MA_21", "feature2", "feature3"])
+        # features = [
+        #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+        #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+        #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+        #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+        #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+        #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+        #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+        #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
+        #     0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, None
+        # ]
+
+        # Convert to NumPy array and reshape
+        input_feature_array = np.array(features).reshape(1, -1)     # shape: (1, 90)
 
         # Ensure the DataFrame has the correct shape and columns
-        if df.shape[1] != 3:
-            raise ValueError("Invalid number of features provided.")
+        # if df.shape[1] != 3:
+        #     raise ValueError("Invalid number of features provided.")
         
         # Add any additional feature engineering steps here
-        return df
+        return input_feature_array
